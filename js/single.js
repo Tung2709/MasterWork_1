018d4ab50cacc3842bf54545b80125fe7e1b6d3b
thucoisao <vuthanhtungautomation@gmail.com>
2022-07-20 22:18:42 +0700

    Source

---
+++ js/single.js
@@ -0,0 +1,22 @@
+
+(
+	function($) {
+	var $window = $(window);
+	var $videoWrap = $('.video-wrap');
+	var $video = $('.video');
+	var videoHeight = $video.outerHeight();
+	$window.on('scroll',  function() {
+		var windowScrollTop = $window.scrollTop();
+		var videoBottom = videoHeight + $videoWrap.offset().top;
+		
+		if (windowScrollTop < (videoBottom-680)) {
+			$videoWrap.height(videoHeight);
+			$video.addClass('stuck');
+		} else {
+			$videoWrap.height('auto');
+			$video.removeClass('stuck');
+			
+		}
+	});
+}(jQuery));
+
